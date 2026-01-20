94d698d390e0f11026e31802c9454a2c587364e0
Andrew <n.huydat2504@gmail.com>
2026-01-20 18:40:55 +0800

    Changed name and implemented skeletal version of Andy

---
+++ src/main/java/Andy.java
@@ -0,0 +1,11 @@
+public class Andy {
+    public static void main(String[] args) {
+        String horizontal = "_________________________________________________________\n";
+        System.out.println(horizontal +
+            "Hello! I'm Andy\n" + 
+            "What can I do for you?\n" +
+            horizontal +
+            "Bye. Hope to see you again soon!\n" +
+            horizontal);
+    }
+}
