02c9d710b01b8586acd55c1295690b7c4f19faf8
Andrew <n.huydat2504@gmail.com>
2026-01-20 19:15:17 +0800

    Implemented echo

--- src/main/java/Andy.java
+++ src/main/java/Andy.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Andy {
     public static void main(String[] args) {
         String horizontal = "_________________________________________________________\n";
-        System.out.println(horizontal +
-            "Hello! I'm Andy\n" + 
-            "What can I do for you?\n" +
-            horizontal +
-            "Bye. Hope to see you again soon!\n" +
-            horizontal);
+        System.out.println(horizontal 
+            + "Hello! I'm Andy\n" 
+            + "What can I do for you?\n" 
+            + horizontal);
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        while(!input.equals("bye")) {
+            System.out.println(horizontal 
+                + formatResponse(input)
+                + horizontal);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(horizontal
+            + formatResponse("Bye. Hope to see you again soon!")
+            + horizontal);
+
+
+        scanner.close();
+    }
+
+    static String formatResponse(String response) {
+        return "\n" 
+        + "\t" 
+        + response 
+        + "\n";
     }
 }
