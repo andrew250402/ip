e874c1448958494007c0ce1b1f9758638aa9a95c
Andrew <n.huydat2504@gmail.com>
2026-01-22 22:02:31 +0800

    Implement Exception Handling

--- src/main/java/Andy.java
+++ src/main/java/Andy.java
@@ -33,18 +33,26 @@ public class Andy {
                     + formatResponse("Ok, I've marked this task as not done yet:\n\t  " + array.get(index - 1))
                     + horizontal);                
             } else {
-                Task newTask = parseInput(input);
-                array.add(newTask);
-                System.out.println(horizontal 
-                    + formatResponse("Got it. I've added this task: \n\t  " 
-                        + newTask
-                        + "\n\tNow you have "
-                        + array.size()
-                        + " task"
-                        + (array.size() == 1 ? "" : "s")
-                        + " in the list."
-                    )
-                    + horizontal);
+                try {
+                    Task newTask = parseInput(input);
+                    array.add(newTask);
+                    System.out.println(horizontal 
+                        + formatResponse("Got it. I've added this task: \n\t  " 
+                            + newTask
+                            + "\n\tNow you have "
+                            + array.size()
+                            + " task"
+                            + (array.size() == 1 ? "" : "s")
+                            + " in the list."
+                        )
+                        + horizontal);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(horizontal
+                        + formatResponse(e.getMessage())
+                        + horizontal
+                    );
+                }
+
  
             }
             input = scanner.nextLine();
@@ -103,19 +111,53 @@ public class Andy {
         int firstSpace = input.indexOf(" ");
         if (first.equals("todo")) {
             String description = input.substring(firstSpace + 1);
+            if (firstSpace == -1) {
+                throw new IllegalArgumentException("Please create a new To do using this format: todo [description]");
+            } else if (description.length() == 0) {
+                throw new IllegalArgumentException("Description is not found. Please add a description for your new To do");
+            }
             task = new Todo(description);
         } else if (first.equals("deadline")) {
             int byIndex = input.indexOf("/by");
+
+
+            if (firstSpace == -1 | byIndex == -1) {
+                throw new IllegalArgumentException("Please create a new Deadline using this format: deadline [description] /by [a deadline]");
+            } 
+
             String description = input.substring(firstSpace + 1, byIndex);
             String by = input.substring(byIndex + 4);
+            
+            if (description.length() == 0) {
+                throw new IllegalArgumentException("Description is not found. Please add a description for your Deadline");
+            } else if (by.length() == 0) {
+                throw new IllegalArgumentException("Deadline is not found. Please add a deadline to your Deadline");
+            }
             task = new Deadline(description, by);
-        } else {
+        } else if (first.equals("event")) {
             int fromIndex = input.indexOf("/from");
             int toIndex = input.indexOf("/to");
+
+            if (firstSpace == -1 | fromIndex == -1 | toIndex == -1) {
+                throw new IllegalArgumentException("Please create a new Event using this format: event [description] /from [a start time] /to [an end time]");
+            } 
+
             String description = input.substring(firstSpace + 1, fromIndex);
             String from = input.substring(fromIndex + 5, toIndex);
             String to = input.substring(toIndex + 4);
+            
+            if (description.length() == 0) {
+                throw new IllegalArgumentException("Description is not found. Please add a description for your Event");
+            } else if (from.length() == 0) {
+                throw new IllegalArgumentException("Start time is not found. Please add a start time to your Event");
+            } else if (to.length() == 0) {
+                throw new IllegalArgumentException("End time is not found. Please add an end time to your Event");
+            }
             task = new Event(description, from, to);
+        } else {
+            throw new IllegalArgumentException("Start with 'todo', 'deadline' or 'event' to add task.\n\t"
+            + "Use 'list' to view your tasks.\n\t" 
+            + "Use 'bye' to stop Andy.");
         }
         return task;
     }
