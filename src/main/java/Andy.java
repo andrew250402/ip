2fd24a48cf90478846e7a6a29bea59efec56d29c
Andrew <n.huydat2504@gmail.com>
2026-01-30 11:59:00 +0800

    Implement Ui

--- src/main/java/Andy.java
+++ src/main/java/Andy.java
@@ -3,101 +3,46 @@ import java.time.format.DateTimeParseException;
 
 public class Andy {
     public static void main(String[] args) {
-        String horizontal = "_________________________________________________________\n";
-        System.out.println(horizontal 
-            + "Hello! I'm Andy\n" 
-            + "What can I do for you?\n" 
-            + horizontal);
-        Scanner scanner = new Scanner(System.in);
 
+        Ui ui = new Ui();
+        ui.start();
+        Scanner scanner = new Scanner(System.in);
         FileParser parser = new FileParser("../../../data/task.txt");
         TaskList array = new TaskList(parser.readFile());
-
         String input = scanner.nextLine();
         InputParser parsedInput = new InputParser(input);
 
-
         while (!parsedInput.isBye()) {
             if (parsedInput.isList()) {
-                System.out.println(horizontal 
-                    + formatList(array)
-                    + horizontal);     
+                ui.listTasks(array);    
             } else if (parsedInput.isMark()) {
                 int index = parsedInput.getIndex();
                 array = array.change(index, true);
-                System.out.println(horizontal
-                    + formatResponse("Nice! I've marked this task as done:\n\t  " + array.get(index))
-                    + horizontal);
+                ui.mark(array, index);
             } else if (parsedInput.isUnmark()) {
                 int index = parsedInput.getIndex();
                 array = array.change(index, false);
-                System.out.println(horizontal
-                    + formatResponse("Ok, I've marked this task as not done yet:\n\t  " + array.get(index))
-                    + horizontal);                
+                ui.unmark(array, index);              
             } else if (parsedInput.isDelete()) {
                 int index = parsedInput.getIndex();
                 Task removed = array.remove(index);
-                System.out.println(horizontal
-                    + formatResponse("Noted, I've removed this task:\n\t  "
-                        + removed
-                        + "\n\tNow you have "
-                        + array.size()
-                        + " task"
-                        + (array.size() == 1 ? "" : "s")
-                        + " in the list."
-                    )
-                    + horizontal
-                );
-
+                ui.remove(array, removed);
             } else {
                 try {
                     Task newTask = parsedInput.getTask();
                     array = array.add(newTask);
-                    System.out.println(horizontal 
-                        + formatResponse("Got it. I've added this task: \n\t  " 
-                            + newTask
-                            + "\n\tNow you have "
-                            + array.size()
-                            + " task"
-                            + (array.size() == 1 ? "" : "s")
-                            + " in the list."
-                        )
-                        + horizontal);
+                    ui.add(array, newTask);
                 } catch (IllegalArgumentException e) {
-                    System.out.println(horizontal
-                        + formatResponse(e.getMessage())
-                        + horizontal
-                    );
+                    ui.error(e);
                 } catch (DateTimeParseException e1) {
-                    System.out.println(horizontal
-                        + formatResponse("Please use a valid format after '/by' like yyyy-mm-dd")
-                        + horizontal
-                    ); 
+                    ui.custom("Please use a valid format after '/by' like yyyy-mm-dd");
                 }
-
- 
             }
             input = scanner.nextLine();
             parsedInput = new InputParser(input);
         }
-
-        System.out.println(horizontal
-            + formatResponse("Bye. Hope to see you again soon!")
-            + horizontal);
-
+        ui.bye();
         parser.writeFile(array);
         scanner.close();
-    }
-
-    static String formatResponse(String response) {
-        return "\n" 
-        + "\t" 
-        + response 
-        + "\n";
-    }
-
-    static String formatList(TaskList array) {
-        String result = "\n\tHere are the tasks in your list:\n";
-        return result + array.listTasks();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,90 @@
+public class Ui {
+    private final static String HORIZONTAL = "_________________________________________________________\n";
+    public Ui() {
+    }
+
+    public void start() {
+        System.out.println(HORIZONTAL
+            + "Hello! I'm Andy\n" 
+            + "What can I do for you?\n" 
+            + HORIZONTAL);
+    }
+
+    public void listTasks(TaskList array) {
+        System.out.println(HORIZONTAL
+            + formatList(array)
+            + HORIZONTAL);  
+    }
+
+    public void mark(TaskList array, int index) {
+        System.out.println(HORIZONTAL
+            + formatResponse("Nice! I've marked this task as done:\n\t  " + array.get(index))
+            + HORIZONTAL);
+    }
+
+    public void unmark(TaskList array, int index) {
+        System.out.println(HORIZONTAL
+            + formatResponse("Ok, I've marked this task as not done yet:\n\t  " + array.get(index))
+            + HORIZONTAL);
+    }
+
+    public void remove(TaskList array, Task removed) {
+        System.out.println(HORIZONTAL
+            + formatResponse("Noted, I've removed this task:\n\t  "
+                + removed
+                + "\n\tNow you have "
+                + array.size()
+                + " task"
+                + (array.size() == 1 ? "" : "s")
+                + " in the list."
+            )
+            + HORIZONTAL
+        );
+    }
+
+    public void add(TaskList array, Task newTask) {
+        System.out.println(HORIZONTAL 
+            + formatResponse("Got it. I've added this task: \n\t  " 
+                + newTask
+                + "\n\tNow you have "
+                + array.size()
+                + " task"
+                + (array.size() == 1 ? "" : "s")
+                + " in the list."
+            )
+            + HORIZONTAL);
+    }
+
+    public void error(Exception e) {
+        System.out.println(HORIZONTAL
+            + formatResponse(e.getMessage())
+            + HORIZONTAL
+        );
+    }
+
+    public void custom(String message) {
+        System.out.println(HORIZONTAL
+            + formatResponse(message)
+            + HORIZONTAL
+        ); 
+    }
+
+    public void bye() {
+        System.out.println(HORIZONTAL
+            + formatResponse("Bye. Hope to see you again soon!")
+            + HORIZONTAL);
+    }
+
+
+    private String formatResponse(String response) {
+        return "\n" 
+        + "\t" 
+        + response 
+        + "\n";
+    }
+
+    private String formatList(TaskList array) {
+        String result = "\n\tHere are the tasks in your list:\n";
+        return result + array.listTasks();
+    }
+}
