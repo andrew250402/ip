a9ea83a1f8c610e6b6f8939e6ab12c37ed75e4a3
Andrew <n.huydat2504@gmail.com>
2026-02-19 17:51:12 +0800

    Reduce Method Length of Andy.java using SLAP

--- src/main/java/andy/Andy.java
+++ src/main/java/andy/Andy.java
@@ -1,7 +1,6 @@
 package andy;
 
 import java.util.Scanner;
-
 import java.time.format.DateTimeParseException;
 
 public class Andy {
@@ -14,96 +13,106 @@ public class Andy {
         this.parser = new FileParser("data/task.txt");
         this.array = new TaskList(parser.readFile());
     }
+
     public static void main(String[] args) {
+        Andy andy = new Andy();
+        andy.run();
+    }
 
-        Ui ui = new Ui();
+    /* =========================
+       HIGH LEVEL PROGRAM FLOW
+       ========================= */
 
-        FileParser parser = new FileParser("data/task.txt");
-        TaskList array = new TaskList(parser.readFile());
-
+    private void run() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println(ui.start());
+
         String input = scanner.nextLine();
         InputParser parsedInput = new InputParser(input);
 
-        System.out.println(ui.start());
-
         while (!parsedInput.isBye()) {
-            if (parsedInput.isList()) {
-                System.out.println(ui.listTasks(array));    
-            } else if (parsedInput.isMark()) {
-                int index = parsedInput.getIndex();
-                array = array.change(index, true);
-                System.out.println(ui.mark(array, index));
-            } else if (parsedInput.isUnmark()) {
-                int index = parsedInput.getIndex();
-                array = array.change(index, false);
-                System.out.println(ui.unmark(array, index));              
-            } else if (parsedInput.isDelete()) {
-                int index = parsedInput.getIndex();
-                Task removed = array.remove(index);
-                System.out.println(ui.remove(array, removed));
-            } else if (parsedInput.isFind()) {
-                String keyword = parsedInput.getArgument();
-                TaskList desiredList = array.find(keyword);
-                System.out.println(ui.listFind(desiredList));
-            } else {
-                try {
-                    Task newTask = parsedInput.getTask();
-                    array = array.add(newTask);
-                    System.out.println(ui.add(array, newTask));
-                } catch (IllegalArgumentException e) {
-                    System.out.println(ui.error(e));
-                } catch (DateTimeParseException e1) {
-                    System.out.println(ui.custom("Please use a valid format after '/by' like yyyy-mm-dd"));
-                }
-            }
-
+            System.out.println(executeCommand(parsedInput));
             input = scanner.nextLine();
             parsedInput = new InputParser(input);
         }
-        
+
+        shutdown(scanner);
+    }
+
+    private void shutdown(Scanner scanner) {
         parser.writeFile(array);
         scanner.close();
         System.out.println(ui.bye());
     }
 
-    /**
-     * Generates a response for the user's chat message.
-     */
+    private String executeCommand(InputParser parsedInput) {
+        if (parsedInput.isList()) {
+            return ui.listTasks(array);
+        }
+
+        if (parsedInput.isMark()) {
+            return handleMark(parsedInput);
+        }
+
+        if (parsedInput.isUnmark()) {
+            return handleUnmark(parsedInput);
+        }
+
+        if (parsedInput.isDelete()) {
+            return handleDelete(parsedInput);
+        }
+
+        if (parsedInput.isFind()) {
+            return handleFind(parsedInput);
+        }
+
+        return handleAddTask(parsedInput);
+    }
+
+    private String handleMark(InputParser input) {
+        int index = input.getIndex();
+        array = array.change(index, true);
+        return ui.mark(array, index);
+    }
+
+    private String handleUnmark(InputParser input) {
+        int index = input.getIndex();
+        array = array.change(index, false);
+        return ui.unmark(array, index);
+    }
+
+    private String handleDelete(InputParser input) {
+        int index = input.getIndex();
+        Task removed = array.remove(index);
+        return ui.remove(array, removed);
+    }
+
+    private String handleFind(InputParser input) {
+        String keyword = input.getArgument();
+        TaskList result = array.find(keyword);
+        return ui.listFind(result);
+    }
+
+    private String handleAddTask(InputParser input) {
+        try {
+            Task newTask = input.getTask();
+            array = array.add(newTask);
+            return ui.add(array, newTask);
+        } catch (IllegalArgumentException e) {
+            return ui.error(e);
+        } catch (DateTimeParseException e) {
+            return ui.custom("Please use a valid format after '/by' like yyyy-mm-dd");
+        }
+    }
+
     public String getResponse(String input) {
         InputParser parsedInput = new InputParser(input);
 
         if (parsedInput.isBye()) {
             parser.writeFile(array);
             return ui.bye();
-        } else if (parsedInput.isList()) {
-            return ui.listTasks(array);    
-        } else if (parsedInput.isMark()) {
-            int index = parsedInput.getIndex();
-            array = array.change(index, true);
-            return ui.mark(array, index);
-        } else if (parsedInput.isUnmark()) {
-            int index = parsedInput.getIndex();
-            array = array.change(index, false);
-            return ui.unmark(array, index);              
-        } else if (parsedInput.isDelete()) {
-            int index = parsedInput.getIndex();
-            Task removed = array.remove(index);
-            return ui.remove(array, removed);
-        } else if (parsedInput.isFind()) {
-            String keyword = parsedInput.getArgument();
-            TaskList desiredList = array.find(keyword);
-            return ui.listFind(desiredList);
-        } else {
-            try {
-                Task newTask = parsedInput.getTask();
-                array = array.add(newTask);
-                return ui.add(array, newTask);
-            } catch (IllegalArgumentException e) {
-                return ui.error(e);
-            } catch (DateTimeParseException e1) {
-                return ui.custom("Please use a valid format after '/by' like yyyy-mm-dd");
-            }
         }
+
+        return executeCommand(parsedInput);
     }
 }
